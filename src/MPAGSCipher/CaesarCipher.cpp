180ba9ef0e7280da1e0b5925239f842652c395de
Stephen <stephen@stephen-vivobook>
2020-11-06 11:34:48 +0000

    Implemented command line args as struct, created class skeleton for CaesarCipher

---
+++ src/MPAGSCipher/CaesarCipher.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+#include "CaesarCipher.hpp"
+
+CaesarCipher::CaesarCipher(const size_t key)
+  : key_{key}
+{
+}
---
+++ src/MPAGSCipher/CaesarCipher.hpp
@@ -0,0 +1,12 @@
+#ifndef MPAGS_CAESARCIPHER_HPP
+#define MPAGS_CAESARCIPHER_HPP
+
+#include <string>
+
+class CaesarCipher {
+public:
+  CaesarCipher(const size_t key);
+  const size_t key_=0;
+};
+
+#endif
